4fe039dd5628ac9b299c54492af971cc75372fe7
sawada <use.github.mail@gmail.com>
2019-12-25 00:02:45 +0900

    modify App.js

--- App.js
+++ App.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-import BodyText from './src/elements/BodyText';
+//import BodyText from './src/elements/BodyText';
 
 export default function App() {
   //以下表示する為のプログラム
   return (
     <View style={styles.container}>
       <View style={styles.appbar}>
-        <View>
         <Text style={styles.appbarTitle}>MEMOT</Text>
-        </View>
       </View>
       <View style={styles.memoList}>
         <View style={styles.memoListItem}>
@@ -55,25 +53,20 @@ const styles = StyleSheet.create({
   },
   //appbarのスタイリング
   appbar: {
-    //絶対的な位置を設定
     position: 'absolute', //絶対的な位置づけ
     top: 0, //上から０
     left: 0, //左から０
     right: 0, //右から０
     height: 80, //高さ８０
-    backgroundColor: '#265366',
+    paddingTop: 30, //文字の位置調整
+    backgroundColor: '#225566',
     justifyContent: 'center', //上下の位置設定（センター）
     alignItems: 'center', //左右方向の位置設定（センター）
-    shadowColor: '#ccc',
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    marginBottom: 10,
-    shadowRadius: 0,
-    shadowOpacity: 1,
-    elevation: 5,
-    zIndex: 1, //表示する優先度(最前面等)を設定する
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 0 }, //影の幅、高さ設定
+    shadowRadius: 0, //影の丸さ設定
+    shadowOpacity: 1, //影の濃さ（１がMAX）
+    elevation: 100, //表示する優先度(高度)を設定する
   },
   //appbarのフォントのスタイリング
   appbarTitle: {
@@ -113,11 +106,16 @@ const styles = StyleSheet.create({
     borderRadius: 24,
     justifyContent: 'center',
     alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: { width: 0.1, height: 0.1 }, //影の幅、高さ設定
+    shadowRadius: 24, //影の丸さ設定
+    shadowOpacity: 1, //影の濃さ（１がMAX）
+    elevation: 10, //表示する優先度(高度)を設定する
   },
   //+ボタンのフォントのスタイリング
   memoAddAddButtonTitle: {
     fontSize: 32,
-    lineHeight: 35,
+    lineHeight: 34,
     color: '#fff',
   },
 });
