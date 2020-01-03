89f363ca6d0a57fa3ccd73750d70eab3ed7c6d6b
sawada <use.github.mail@gmail.com>
2020-01-04 02:06:27 +0900

    vulnerability found

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 
 import MemoList from './src/components/MemoList.js';
 import Appbar from './src/components/Appbar.js';
