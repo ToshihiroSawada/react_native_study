a0380dea673fa7e8e6f7ecef4e8c3482cb75a6c3
ToshihiroSawada <use.github.mail@gmail.com>
2020-02-19 22:53:59 +0900

    modify LoginScreen.js

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import { StyleSheet, View, Text, TextInput, TouchableHighlight } from 'react-native';
+import firebase from 'firebase';
 
 class LoginScreen extends React.Component {
   state = {
     email: '',
     password: '',
   }
+
+    //ログイン機能の実装
+    handleSubmit() {
+      firebase.auth().signInWithEmailAndPassword(this.state.email, this.state.password)
+        .then((user) => {
+          console.log('succcess!!!!!!!!!\n', user);
+          this.props.navigation.navigate('Home');
+        })
+
+        .catch((error) => {
+          console.log(error);
+        });
+    }
 
   render() {
     return (
